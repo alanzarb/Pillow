588f93b4e45714d3e558ed6d264a8d4bc1b3679f
Alan HP <alan@zarblet.comm>
2020-03-01 21:27:42 -0500

    d3

--- hello.py
+++ hello.py
@@ -19,9 +19,12 @@ resp = requests.get(image_url, stream=True)
 
 # Open a local file with wb ( write binary ) permission.
 local_file = open('local_image2.jpg', 'wb')
+
 # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
 resp.raw.decode_content = True
+
 # Copy the response stream raw data to local image file.
 shutil.copyfileobj(resp.raw, local_file)
+
 # Remove the image url response object.
 del resp
