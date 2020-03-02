9e04d62fdf58e0e33c8c773acccf928eff304729
Alan HP <alan@zarblet.comm>
2020-03-01 21:27:08 -0500

    d2

--- hello.py
+++ hello.py
@@ -3,10 +3,13 @@ import hyperLinks as hl
 
 import requests
 import shutil
+import urllib3
 
-im = Image.open("il_fullxfull.977078474_51xi.jpg")
+# im = Image.open("il_fullxfull.977078474_51xi.jpg")
+im = Image.open(requests.get(hl.p1, stream=True).raw)
+
 print(im.format, im.size, im.mode)
-# im.show()
+im.show()
 
 # This is the image url.
 image_url = hl.p3
@@ -15,7 +18,7 @@ image_url = hl.p3
 resp = requests.get(image_url, stream=True)
 
 # Open a local file with wb ( write binary ) permission.
-local_file = open('local_image.jpg', 'wb')
+local_file = open('local_image2.jpg', 'wb')
 # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
 resp.raw.decode_content = True
 # Copy the response stream raw data to local image file.
