5b3f503bd53f5e878a605b74b06ed6eaee70f757
Alan HP <alan@zarblet.comm>
2020-03-01 21:12:03 -0500

    d1

---
+++ hello.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import hyperLinks as hl
+
+import requests
+import shutil
+
+im = Image.open("il_fullxfull.977078474_51xi.jpg")
+print(im.format, im.size, im.mode)
+# im.show()
+
+# This is the image url.
+image_url = hl.p3
+
+# Open the url image, set stream to True, this will return the stream content.
+resp = requests.get(image_url, stream=True)
+
+# Open a local file with wb ( write binary ) permission.
+local_file = open('local_image.jpg', 'wb')
+# Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+resp.raw.decode_content = True
+# Copy the response stream raw data to local image file.
+shutil.copyfileobj(resp.raw, local_file)
+# Remove the image url response object.
+del resp
---
+++ hyperLinks.py
@@ -0,0 +1,5 @@
+# links to etsyonsale
+p1 = "https://i.etsystatic.com/9874654/r/il/a58c83/977078474/il_fullxfull.977078474_51xi.jpg"
+p2 = "https://i.etsystatic.com/9874654/r/il/dc47d5/779554897/il_fullxfull.779554897_b217.jpg"
+p3 = "https://i.etsystatic.com/9874654/r/il/71cb0b/968644052/il_fullxfull.968644052_38xd.jpg"
+p4 = "https://i.etsystatic.com/9874654/r/il/7567a8/1477324885/il_fullxfull.1477324885_l3vk.jpg"
