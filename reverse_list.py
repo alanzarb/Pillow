8df215397054045f441c1d905d8f40c00f29fc48
Alan HP <alan@zarblet.comm>
2020-03-08 15:47:38 -0400

    reverse3

--- reverse_list.py
+++ reverse_list.py
@@ -7,10 +7,10 @@ def reverse_list(elems):
     new_list = []
     for i in range(len(elems)):
         # (Fill in the missing line here)
-    return new_list
+        return new_list
 
 
 if __name__ == "__main__":
     lst = [1, 4, 7, 9, 12, 14]
     print(reverse_list(lst))
-    
+
